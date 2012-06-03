e8ec1f8b21270f6fb3b3edf7ec8845373a172496
Matt Thomas <matt@betweenbrain.com>
2012-06-03 16:19:15 -0400

    Fixing missing definition of $buffer

---
+++ htmloptimize.php
@@ -0,0 +1,32 @@
+<?php defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+class plgSystemHtmloptimize extends JPlugin
+{
+
+	function plgSystemHtmloptimize(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+	}
+
+	function onAfterRender()
+	{
+		$app = JFactory::getApplication();
+
+		if ($app->isAdmin()) {
+			return;
+		}
+
+		$buffer = JResponse::getBody();
+
+		// Remove HTML comments, double new lines, double carriage returns and tabs.
+		$buffer = preg_replace("/<!--[a-zA-Z0-9- ]*-->|[\n|\r]{2,}|\t/","",$buffer);
+
+		JResponse::setBody($buffer);
+
+		return true;
+		
+	}
+}
+
