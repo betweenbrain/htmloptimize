564ec96c93ecdb9f906afb3ed5b96c7ba54549b5
Matt Thomas <matt@betweenbrain.com>
2012-06-03 20:21:47 -0400

    Added a number of optimizations, while maintaining readability of the code. Written verbosely.

--- htmloptimize.php
+++ htmloptimize.php
@@ -21,7 +21,25 @@ class plgSystemHtmloptimize extends JPlugin
 		$buffer = JResponse::getBody();
 
 		// Remove HTML comments, double new lines, double carriage returns and tabs.
-		$buffer = preg_replace("/<!--[a-zA-Z0-9- ]*-->|[\n|\r]{2,}|\t/","",$buffer);
+		// $buffer = preg_replace("/<!--[a-zA-Z0-9-.\/ ]*-->|[\n|\r]{2,}|\t/","",$buffer);
+
+		// Remove HTML comments
+		$buffer = preg_replace("/<!--[a-zA-Z0-9-.\/ ]*-->/","",$buffer);
+
+		// Replace double new lines, double carriage returns with single.
+		$buffer = preg_replace("/([\n|\r]){2,}/","$1",$buffer);
+
+		// Remove tabs
+		$buffer = preg_replace("/\t/","",$buffer);
+
+		// Remove double or more leading spaces at beginning of lines.
+		$buffer = preg_replace("/(\n)\s{2,}/","$1",$buffer);
+
+		// Remove empty lines.
+		$buffer = preg_replace("/(\n)[\s\t]+</","$1<",$buffer);
+
+		// Replace double spaces with single.
+		$buffer = preg_replace("/\s{2,}/"," ",$buffer);
 
 		JResponse::setBody($buffer);
 
