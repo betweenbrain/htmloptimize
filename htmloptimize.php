f9e777b2d8cd9ca33e6804e6e636062a7e601a03
betweenbrain <matt@betweenbrain.com>
2012-07-01 00:02:32 -0400

    Revising regex that targeted double carriage returns and new lines, which also targeted double bar characters and broke some Google Analytics code.

--- htmloptimize.php
+++ htmloptimize.php
@@ -21,27 +21,30 @@ class plgSystemHtmloptimize extends JPlugin
 		$buffer = JResponse::getBody();
 
 		// Remove HTML comments
-		$buffer = preg_replace("/<!--[a-zA-Z0-9-.\/ ]*-->/","",$buffer);
+		$buffer = preg_replace("/<!--[a-zA-Z0-9-.\/ ]*-->/", "", $buffer);
 
-		// Replace double new lines, double carriage returns with single occurrence.
-		$buffer = preg_replace("/([\n|\r]){2,}/","$1",$buffer);
+		// Replace double carriage returns with single.
+		$buffer = preg_replace("/\r{2,}/", "\r", $buffer);
+
+		// Replace double new lines with single.
+		$buffer = preg_replace("/\n{2,}/", "\n", $buffer);
 
 		// Remove tabs (Tabs?! We don't need no stinkin' tabs!)
-		$buffer = preg_replace("/\t/","",$buffer);
+		$buffer = preg_replace("/\t/", "", $buffer);
 
 		// Remove two or more leading spaces at the beginning of lines.
-		$buffer = preg_replace("/(\n)\s{2,}/","$1",$buffer);
+		$buffer = preg_replace("/(\n)\s{2,}/", "$1", $buffer);
 
 		// Remove empty lines.
-		$buffer = preg_replace("/(\n)[\s\t]+</","$1<",$buffer);
+		$buffer = preg_replace("/(\n)[\s\t]+</", "$1<", $buffer);
 
 		// Replace double spaces with single.
-		$buffer = preg_replace("/\s{2,}/"," ",$buffer);
+		$buffer = preg_replace("/\s{2,}/", " ", $buffer);
 
 		JResponse::setBody($buffer);
 
 		return true;
-
 	}
 }
+
 
